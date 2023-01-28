4b78fe3b107d40051573780608a7161131581221
AronLee5263 <aronlee5263@gmail.com>
2023-01-28 23:21:02 +0900

    그냥 병합을 못해서 이전 커밋에서 파일 두개 내용 복사해서 붙여넣고 새롭게 master 브랜치에서 커밋함.

--- src/component/Modal.js
+++ src/component/Modal.js
@@ -1,11 +1,24 @@
-function Modal() {
+function Modal(props) {
+  function cancelHandler() {
+    props.onCancel();
+  }
+
+  function confirmHandler() {
+    props.onConfirm();
+  }
+
   return (
     <div className="modal">
       <p>Are you sure?</p>
-      <button className="btn btn--alt">Cancle</button>
-      <button className="btn">Confirm</button>
+      <button className="btn btn--alt" onClick={cancelHandler}>
+        Cancle
+      </button>
+      <button className="btn" onClick={confirmHandler}>
+        Confirm
+      </button>
     </div>
   );
 }
 
 export default Modal;
+//그냥 두개 파일 복사해서 다시 마스터 브랜치에서 커밋 푸쉬하게..
